4e96178cefb9ea1e1ed94401921f9b17c5545f27
WDF <wdf_jt@163.com>
2016-09-23 13:35:53 +0800

    init

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java $JAVA_OPTS  -cp target/wx.jar com.jfinal.weixin.demo.WeixinConfig
--- src/main/java/com/jfinal/weixin/demo/WeixinConfig.java
+++ src/main/java/com/jfinal/weixin/demo/WeixinConfig.java
@@ -1,9 +1,11 @@
 /**
-  * Copyright (c) 2015-2016, Javen Zhou  (javenlife@126.com).
+ * Copyright (c) 2011-2014, James Zhan 詹波 (jfinal@126.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  */
+
 package com.jfinal.weixin.demo;
+
 import com.jfinal.config.Constants;
 import com.jfinal.config.Handlers;
 import com.jfinal.config.Interceptors;
@@ -12,80 +14,96 @@ import com.jfinal.config.Plugins;
 import com.jfinal.config.Routes;
 import com.jfinal.core.JFinal;
 import com.jfinal.kit.PropKit;
-import com.jfinal.render.ViewType;
+import com.jfinal.weixin.sdk.api.ApiConfig;
 import com.jfinal.weixin.sdk.api.ApiConfigKit;
-import com.jfinal.weixin.share.ShareController;
-import com.jfinal.weixin.usercontroller.UserController;
-import com.jfinal.weixin.weboauth2.RedirectUri;
-/**
- * @author Javen
- * 2016年1月15日
- */
+
 public class WeixinConfig extends JFinalConfig {
-	
-	/**
-	 * 如果生产环境配置文件存在，则优先加载该配置，否则加载开发环境配置文件
-	 * @param pro 生产环境配置文件
-	 * @param dev 开发环境配置文件
-	 */
-	public void loadProp(String pro, String dev) {
-		try {
-			PropKit.use(pro);
-		}
-		catch (Exception e) {
-			PropKit.use(dev);
-		}
-	}
-	
-	public void configConstant(Constants me) {
-		loadProp("a_little_config_pro.txt", "a_little_config.txt");
-		me.setDevMode(PropKit.getBoolean("devMode", false));
-		me.setEncoding("utf-8");
-		me.setViewType(ViewType.JSP);
-		// ApiConfigKit 设为开发模式可以在开发阶段输出请求交互的 xml 与 json 数据
-		ApiConfigKit.setDevMode(me.getDevMode());
-	}
-	
-	public void configRoute(Routes me) {
-		me.add("/msg", WeixinMsgController.class);
-		me.add("/api", WeixinApiController.class, "/api");
-		me.add("/pay", WeixinPayController.class);
-		me.add("/", IndexController.class,"/index");
-		me.add("/oauth2",RedirectUri.class);
-		me.add("/user",UserController.class);
-		me.add("/jssdk",ShareController.class,"_front");
 
-	}
-	
-	public void configPlugin(Plugins me) {
-		// C3p0Plugin c3p0Plugin = new C3p0Plugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password").trim());
-		// me.add(c3p0Plugin);
-		
-		// EhCachePlugin ecp = new EhCachePlugin();
-		// me.add(ecp);
-		
-		// RedisPlugin redisPlugin = new RedisPlugin("weixin", "127.0.0.1");
-		// me.add(redisPlugin);
-	}
-	
-	public void configInterceptor(Interceptors me) {
-		
-	}
-	
-	public void configHandler(Handlers me) {
-		
-	}
-	
-	public void afterJFinalStart() {
-		// 1.5 之后支持redis存储access_token、js_ticket，需要先启动RedisPlugin
-//		ApiConfigKit.setAccessTokenCache(new RedisAccessTokenCache());
-		// 1.6新增的2种初始化
-//		ApiConfigKit.setAccessTokenCache(new RedisAccessTokenCache(Redis.use("weixin")));
-//		ApiConfigKit.setAccessTokenCache(new RedisAccessTokenCache("weixin"));
-	}
+    /**
+     * 如果生产环境配置文件存在，则优先加载该配置，否则加载开发环境配置文件
+     * @param pro 生产环境配置文件
+     * @param dev 开发环境配置文件
+     */
+    public void loadProp(String pro, String dev) {
+        try {
+            PropKit.use(pro);
+        }
+        catch (Exception e) {
+            PropKit.use(dev);
+        }
+    }
 
-	public static void main(String[] args) {
-		JFinal.start("src/main/webapp", 8080, "/", 5);
-	}
-	
+    public void configConstant(Constants me) {
+        loadProp("a_little_config_pro.txt", "a_little_config.txt");
+        me.setDevMode(PropKit.getBoolean("devMode", false));
+
+        // ApiConfigKit 设为开发模式可以在开发阶段输出请求交互的 xml 与 json 数据
+        ApiConfigKit.setDevMode(me.getDevMode());
+    }
+
+    public void configRoute(Routes me) {
+        me.add("/msg", WeixinMsgController.class);
+        me.add("/api", WeixinApiController.class, "/api");
+        me.add("/pay", WeixinPayController.class);
+    }
+
+    public void configPlugin(Plugins me) {
+        // C3p0Plugin c3p0Plugin = new C3p0Plugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password").trim());
+        // me.add(c3p0Plugin);
+
+        // EhCachePlugin ecp = new EhCachePlugin();
+        // me.add(ecp);
+
+        // 使用redis分布accessToken
+        // RedisPlugin redisPlugin = new RedisPlugin("weixin", "127.0.0.1");
+        // redisPlugin.setSerializer(JdkSerializer.me); // 需要使用fst高性能序列化的用户请删除这一行（Fst jar依赖请查看WIKI）
+        // me.add(redisPlugin);
+    }
+
+    public void configInterceptor(Interceptors me) {
+        // 设置默认的 appId 规则，默认值为appId，可采用url挂参数 ?appId=xxx 切换多公众号
+        // ApiInterceptor.setAppIdParser(new AppIdParser.DefaultParameterAppIdParser("appId")); 默认无需设置
+        // MsgInterceptor.setAppIdParser(new AppIdParser.DefaultParameterAppIdParser("appId")); 默认无需设置
+    }
+
+    public void configHandler(Handlers me) {
+
+    }
+
+    public void afterJFinalStart() {
+        // 1.5 之后支持redis存储access_token、js_ticket，需要先启动RedisPlugin
+//        ApiConfigKit.setAccessTokenCache(new RedisAccessTokenCache());
+        // 1.6新增的2种初始化
+//        ApiConfigKit.setAccessTokenCache(new RedisAccessTokenCache(Redis.use("weixin")));
+//        ApiConfigKit.setAccessTokenCache(new RedisAccessTokenCache("weixin"));
+
+        ApiConfig ac = new ApiConfig();
+        // 配置微信 API 相关参数
+        ac.setToken(PropKit.get("token"));
+        ac.setAppId(PropKit.get("appId"));
+        ac.setAppSecret(PropKit.get("appSecret"));
+
+        /**
+         *  是否对消息进行加密，对应于微信平台的消息加解密方式：
+         *  1：true进行加密且必须配置 encodingAesKey
+         *  2：false采用明文模式，同时也支持混合模式
+         */
+        ac.setEncryptMessage(PropKit.getBoolean("encryptMessage", false));
+        ac.setEncodingAesKey(PropKit.get("encodingAesKey", "setting it in config file"));
+
+        /**
+         * 多个公众号时，重复调用ApiConfigKit.putApiConfig(ac)依次添加即可，第一个添加的是默认。
+         */
+        ApiConfigKit.putApiConfig(ac);
+        
+        // 微信 WxSession的配置
+        // 启用默认的Session管理器
+//        ApiConfigKit.enableDefaultWxSessionManager();
+        // 启用redis Session管理器
+//        ApiConfigKit.setWxSessionManager(new RedisWxSessionManager("weixin"));
+    }
+
+    public static void main(String[] args) {
+        JFinal.start("src/main/webapp", 80, "/", 5);
+    }
 }
